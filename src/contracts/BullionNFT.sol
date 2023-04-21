4f2a637ac73b12f540397ec22108b3a677974ed0
devmaster54 <soft.expert32@gmail.com>
2023-04-21 12:25:14 -0500

    update

--- src/contracts/BullionNFT.sol
+++ src/contracts/BullionNFT.sol
@@ -7,61 +7,26 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract BullionTribeClub is ERC721, Ownable {
   using SafeMath for uint256;
 
-  uint256 public tribePrice = 80000000000000000; //0.08 ETH
-
-  uint public maxTribePurchase = 20;
-
   uint256 public MAX_TRIBES;
-
-  bool public saleIsActive = false;
-
-  uint256 public REVEAL_TIMESTAMP;
 
   string public nftBaseURI;
 
   uint public totalSupply;
 
-  constructor(
-    uint256 maxNftSupply,
-    uint256 saleStart
-  ) ERC721("Bullion Tribe", "TRIBE") {
+  constructor(uint256 maxNftSupply) ERC721("Bullion Tribe", "TRIBE") {
     MAX_TRIBES = maxNftSupply;
-    REVEAL_TIMESTAMP = saleStart + (86400 * 9);
-  }
-
-  function withdraw() public onlyOwner {
-    uint balance = address(this).balance;
-    payable(msg.sender).transfer(balance);
-  }
-
-  /**
-   * Set some Bullion Tribes aside
-   */
-  function reserveTribes() public onlyOwner {
-    uint i;
-    for (i = 0; i < 30; i++) {
-      _safeMint(msg.sender, totalSupply + i);
-      totalSupply++;
-    }
-  }
-
-  /**
-   * DM Gargamel in Discord that you're standing right behind him.
-   */
-  function setRevealTimestamp(uint256 revealTimeStamp) public onlyOwner {
-    REVEAL_TIMESTAMP = revealTimeStamp;
-  }
-
-  function setTribePrice(uint256 price) public onlyOwner {
-    tribePrice = price;
-  }
-
-  function setMaxTribePurchase(uint256 purchase) public onlyOwner {
-    maxTribePurchase = purchase;
   }
 
   function setBaseURI(string memory baseURI) public onlyOwner {
     _setBaseURI(baseURI);
+  }
+
+  function setMaxTribes(uint256 maxNftSupply) public onlyOwner {
+    require(
+      totalSupply <= MAX_TRIBES,
+      "MAX_TRIBES should be greater than TotalSupply"
+    );
+    MAX_TRIBES = maxNftSupply;
   }
 
   function _setBaseURI(string memory baseURI) internal virtual {
@@ -72,37 +37,18 @@ contract BullionTribeClub is ERC721, Ownable {
     return nftBaseURI;
   }
 
-  /*
-   * Pause sale if active, make active if paused
-   */
-  function flipSaleState() public onlyOwner {
-    saleIsActive = !saleIsActive;
-  }
-
   /**
    * Mints Bullion Tribes
    */
-  function mintTribe(uint numberOfTokens) public payable {
-    require(saleIsActive, "Sale must be active to mint Tribe");
-    require(
-      numberOfTokens <= maxTribePurchase,
-      "Can only mint 20 tokens at a time"
-    );
+  function mintTribe(uint256 numberOfTokens) public payable {
     require(
       totalSupply.add(numberOfTokens) <= MAX_TRIBES,
-      "Purchase would exceed max supply of Tribes"
-    );
-    require(
-      tribePrice.mul(numberOfTokens) <= msg.value,
-      "Ether value sent is not correct"
+      "Mint would exceed max supply of Tribes"
     );
 
-    for (uint i = 0; i < numberOfTokens; i++) {
-      uint mintIndex = totalSupply;
-      if (totalSupply < MAX_TRIBES) {
-        _safeMint(msg.sender, mintIndex);
-        totalSupply++;
-      }
+    for (uint i = 1; i <= numberOfTokens; i++) {
+      _safeMint(msg.sender, totalSupply.add(i));
     }
+    totalSupply += numberOfTokens;
   }
 }
